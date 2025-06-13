96c1dd9cefea60e57d029b92e4d7639d152f7840
ademguney <adem.guney@outlook.com.tr>
2025-06-13 22:16:26 +0300

    feat(core, tests): implement basic Mediator send logic with successful integration test.

---
+++ tests/MediatRRise.Tests/Integration/MediatorTests.cs
@@ -0,0 +1,39 @@
+﻿using MediatRRise.Core.Abstractions;
+using MediatRRise.Infrastructure.Implemantation;
+using MediatRRise.Tests.TestFixtures;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MediatRRise.Tests.Integration;
+
+public class MediatorTests
+{
+    private readonly IMediator _mediator;
+
+    public MediatorTests()
+    {
+        var services = new ServiceCollection();
+
+        // Register PingQueryHandler
+        services.AddTransient<IRequestHandler<PingQuery, string>, PingQueryHandler>();
+
+        // Register MediatRRise
+        services.AddSingleton<IMediator, Mediator>();
+
+        var provider = services.BuildServiceProvider();
+        _mediator = provider.GetRequiredService<IMediator>();
+    }
+
+
+    [Fact]
+    public async Task Send_Should_Invoke_Handler_And_Return_Result()
+    {
+        // Arrange
+        var query = new PingQuery("Hello, MediatR Rise!");
+
+        // Act
+        var result = await _mediator.Send(query);
+
+        // Assert
+        Assert.Equal("Pong: Hello, MediatR Rise!", result);
+    }
+}
---
+++ tests/MediatRRise.Tests/TestFixtures/PingQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatRRise.Core.Abstractions;
+
+namespace MediatRRise.Tests.TestFixtures;
+public record PingQuery(string Message) : IRequest<string>;
+
+public class PingQueryHandler : IRequestHandler<PingQuery, string>
+{
+    public Task<string> Handle(PingQuery request, CancellationToken cancellationToken)
+    {
+        return Task.FromResult($"Pong: {request.Message}");
+    }
+}
